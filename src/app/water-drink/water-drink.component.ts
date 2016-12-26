7333029dbdd3e94a79625438e1d0cc313affb680
Steve Zheng <steve.zheng@globallogic.com>
2016-12-26 10:20:33 -0500

    use scss

---
+++ src/app/water-drink/water-drink.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-water-drink',
+  templateUrl: './water-drink.component.html',
+  styleUrls: ['./water-drink.component.scss']
+})
+export class WaterDrinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
