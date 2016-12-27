f47ae9f8c8b9bfac8f4e6a4e7c3b11768d2fd882
Steve Zheng <steve.zheng@globallogic.com>
2016-12-27 15:29:15 -0500

    added dashboard module and cmps inside it and imported font-awesome

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DashboardContainerComponent } from './dashboard/dashboard-container/dashboard-container.component';
 import { WaterDrinkComponent } from './water/water-drink/water-drink.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'water', pathMatch: 'full' },
-  { path: 'water', component: WaterDrinkComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardContainerComponent },
+  { path: 'water', component: WaterDrinkComponent}
 ];
 
 @NgModule({
